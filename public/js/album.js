024b430992633ace53edb0a96c6d705a60ac501f
dknik <dknikolajk@gmail.com>
2023-12-12 13:00:37 +0100

    albums pictures added

--- public/js/album.js
+++ public/js/album.js
@@ -30,6 +30,7 @@ async function renderAlbums () {
             console.log(album);
             appendAlbums(album.data)
         });
+        renderAlbumPictures()
     }
     else {
         await fetch('/albums', {
@@ -49,6 +50,7 @@ async function renderAlbums () {
                     console.log(album);
                     appendAlbums(album.data)
                 });
+                renderAlbumPictures()
             });
     }
 }
@@ -57,15 +59,46 @@ function appendAlbums(album){
     const albumDiv = document.createElement('div');
     albumDiv.setAttribute('class','album-object')
     const albumName = document.createElement('p');
+    albumName.setAttribute('class','p-album-name')
     albumName.textContent = album.album_name;
     albumDiv.appendChild(albumName);
     const albumBtn = document.createElement('button')
     albumBtn.setAttribute('class','changBtn')
     albumBtn.textContent = 'Change Name'
     albumDiv.appendChild(albumBtn)
-    albums.appendChild(albumDiv);
+    albums.appendChild(albumDiv)
 
 }
+function renderAlbumPictures () {
+    const sessionPictures = JSON.parse(sessionStorage.getItem("sessionPictures"))
+    console.log(sessionPictures)
+    const sessionAlbums = JSON.parse(sessionStorage.getItem("sessionAlbums"))
+    console.log(sessionAlbums)
+    const pTagAlbums = document.querySelectorAll('.p-album-name');
+
+    // for each pic check if the album id fits
+    sessionPictures.forEach(picture => {
+        console.log(picture.album_id)
+        sessionAlbums.forEach(album => {
+            if (picture.album_id === album.id) {
+                pTagAlbums.forEach(tag =>{
+                    console.log(tag)
+                    if (tag.innerText === album.data.album_name) {
+                        const img = document.createElement('img')
+                        img.src = picture.img_path
+                        tag.insertAdjacentElement('afterend',img)
+                    }
+                })
+            }
+        })
+    })
+
+    // use if else to check all the different album ids
+    // if the pic has the album id, find the right p element with the album name that is connected to the album id
+    // add img element after the p element
+}
+
+
 
 const uploadAlbumModal = document.querySelector('#upload-album-modal');
 const uploadAlbumBtn = document.querySelector('#upload-album-button');
